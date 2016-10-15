3e6f4f2793765bb998c76a0e9dab863d18912670
Truc Chinh Dao <truc.c.dao@gmail.com>
2016-10-15 16:03:28 +0700

    add my own app ids & api keys

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -12,10 +12,10 @@ public class BuildVars {
     public static boolean DEBUG_VERSION = false;
     public static int BUILD_VERSION = 851;
     public static String BUILD_VERSION_STRING = "3.13";
-    public static int APP_ID = 0; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
-    public static String APP_HASH = ""; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
-    public static String HOCKEY_APP_HASH = "your-hockeyapp-api-key-here";
-    public static String HOCKEY_APP_HASH_DEBUG = "your-hockeyapp-api-key-here";
+    public static int APP_ID = 95368; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+    public static String APP_HASH = "bbe393839be1fd40e76e6642dc2a0f35"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+    public static String HOCKEY_APP_HASH = "5b9c282b50ef434fa86ef454e98c7d07"; // obtain your own hockeyapp api key at https://rink.hockeyapp.net/
+    public static String HOCKEY_APP_HASH_DEBUG = "5b9c282b50ef434fa86ef454e98c7d07"; // obtain your own hockeyapp api key at https://rink.hockeyapp.net/
     public static String GCM_SENDER_ID = "760348033672";
     public static String SEND_LOGS_EMAIL = "email@gmail.com";
     public static String BING_SEARCH_KEY = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
